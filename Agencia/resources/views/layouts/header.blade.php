da9979fe60040e0b02e3576d4ce817662f1f7fb6
VEGA MERCADO Martin <martinvegamercado@gmail.com>
2022-04-19 10:11:49 -0300

    swett alert

--- Agencia/resources/views/layouts/header.blade.php
+++ Agencia/resources/views/layouts/header.blade.php
@@ -7,7 +7,8 @@
     <title>PHP Laravel</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+     <link href="//cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@4/dark.css" rel="stylesheet">
+     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
