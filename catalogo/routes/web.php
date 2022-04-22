861ea51b0717695c7ced8d6063d1ff98d08e531f
Martin Vega Mercado <martinvegamercado@gmail.com>
2022-04-21 22:02:47 -0300

    catalogo app

---
+++ catalogo/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//ruteo sin metodo con vista directa
+Route::view('/inicio', 'inicio');
+
+####crud de marcas ######
+
+use App\Http\Controllers\MarcaController;
+
+
+Route::get('/marcas',[MarcaController::class,'index']);
